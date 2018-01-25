99b8adda7161bb3d32745f7e2e276d8d94bf5199
Sohva <sofia.h.e.simola@gmail.com>
2018-01-25 18:04:02 +0000

    The end of 5.7

--- rango/models.py
+++ rango/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
     
-def __str__(self): # For Python 2, use __unicode__ too
-    return self.name
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.name
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
@@ -13,5 +16,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
     
-def __str__(self): # For Python 2, use __unicode__ too
-    return self.title
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.title
